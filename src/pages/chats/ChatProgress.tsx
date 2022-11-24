cc815296b68a9eb34729e47b04a40b7b11f1f6f3
rifatptk <rifatptk@gmail.com>
2022-11-24 11:32:38 +0600

    chat main header added

--- src/pages/chats/ChatProgress.tsx
+++ src/pages/chats/ChatProgress.tsx
@@ -1,121 +1,33 @@
+import avatar from '../../assets/images/userProfilePage/avatar.png';
+import arrow from '../../assets/images/chatspage/darr.png';
+import { useState } from 'react';
+
 type Props = any;
 
 const ChatProgress = ({ profile }: Props) => {
+  const [isExpanded, setisExpanded] = useState(false);
   return (
-    <div>
-      ChatProgress with {profile.username}
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ad
-        voluptate, atque earum possimus incidunt, obcaecati impedit similique
-        inventore, veniam at! Veritatis eos, adipisci doloremque dolorem quam
-        distinctio quod dicta doloribus alias nostrum, ullam modi odit saepe
-        porro obcaecati laudantium quibusdam et nesciunt quae. Atque architecto
-        minima officiis eum molestiae vitae, excepturi sequi dicta ipsa
-        doloremque magnam? Soluta magnam modi culpa. Ducimus sed tempore, sint
-        cum vero incidunt! Deserunt, pariatur. Natus, quibusdam asperiores, eos
-        ipsam velit vel ut rerum quae ea illum nemo praesentium cupiditate neque
-        consectetur sed quaerat nobis quia aperiam blanditiis dicta ducimus,
-        voluptatem quas corrupti. Distinctio odit soluta fuga optio ullam ex
-        sunt, inventore cumque molestias cum repellat repellendus dolore quae
-        velit iste qui exercitationem voluptate voluptas! Eum obcaecati, facere
-        consequuntur similique aspernatur temporibus praesentium dignissimos?
-        Hic, quasi nemo deleniti veniam voluptatum nulla placeat eius earum,
-        consequatur vel, harum modi unde rerum quia quod. Sequi iste sint enim
-        dignissimos quae laboriosam. Cum consequatur laborum similique tempora
-        id neque, eaque ipsa fugiat dolorem, blanditiis, vitae sed eum dicta
-        cupiditate quibusdam eligendi. Quas id vitae nisi quidem quisquam sit
-        cupiditate recusandae architecto. Dolorem, obcaecati id fuga quia quidem
-        natus, iure voluptatem nostrum est consequuntur labore iusto laudantium
-        officiis rerum, porro veritatis nisi. Veritatis aspernatur officiis
-        ipsum voluptas! Vel autem culpa nesciunt dolorem quos, accusamus quod
-        cum illo? Inventore dolore quo dicta error illo commodi fugit nesciunt
-        expedita sit placeat hic quod saepe, reprehenderit rem tempore
-        temporibus ipsam quaerat vitae cum. Eius veritatis quidem ullam quaerat
-        tempore voluptates doloribus hic totam, optio laudantium? Hic aperiam
-        aliquid eveniet esse possimus exercitationem sapiente dolorum,
-        reprehenderit ipsam iusto, optio similique quo ullam neque doloribus
-        quam temporibus, maxime mollitia. Porro voluptas consectetur iste harum
-        obcaecati voluptatum eos, odio alias incidunt, vitae eius magni?
-        Explicabo consequuntur repudiandae sed est esse unde ratione iure
-        pariatur quam a nesciunt facilis porro fuga praesentium et, modi in
-        molestiae ad, mollitia fugiat, delectus id. Doloribus voluptates error
-        ratione minus dolorum temporibus, tempora esse maxime optio soluta
-        laudantium perspiciatis ipsam itaque excepturi. Velit quam excepturi non
-        aspernatur quasi blanditiis asperiores, sunt facilis accusamus. Enim eos
-        earum nobis itaque tempore facilis rem quas laborum aspernatur hic,
-        impedit veritatis perspiciatis expedita deserunt neque nisi laboriosam
-        sapiente amet molestiae id. Ex, maxime ipsa pariatur doloremque
-        reprehenderit placeat soluta mollitia veniam numquam corporis
-        aspernatur? Maxime reprehenderit quo placeat nobis nisi necessitatibus
-        doloremque iure quae architecto error possimus ducimus, ex assumenda
-        vero aperiam explicabo ullam dolores, repudiandae saepe at. Ipsum
-        delectus reprehenderit quibusdam itaque possimus, cum totam non cumque
-        sapiente quae obcaecati quo expedita quaerat rerum dignissimos! Expedita
-        deleniti velit exercitationem quasi sapiente corrupti nesciunt optio
-        dignissimos excepturi dolor incidunt officiis modi harum nulla error,
-        odit repudiandae voluptatibus voluptates eum accusamus, numquam eligendi
-        minus labore. Eum dolor officia pariatur mollitia illum doloribus
-        aliquam officiis maiores perferendis quam tempora voluptatum, dolorum
-        tenetur ex perspiciatis fuga, magni sed! Sequi corrupti, facere,
-        ducimus, quis nulla nemo dolor dolorum eius sed explicabo officiis iure
-        culpa minus ullam deleniti ratione dicta reiciendis. Nam, quae.
-        Inventore obcaecati commodi atque saepe, quaerat recusandae. Vitae eum
-        possimus culpa. Fugiat consectetur libero modi officia aperiam commodi
-        aut voluptatem, ratione error! Hic sint corrupti iure, voluptatibus a
-        illo molestiae quas dolores, laudantium repudiandae facilis ducimus
-        perspiciatis. Fuga perspiciatis, at laudantium quam in esse nam dolorem
-        doloremque totam atque doloribus! Eos autem sit eius labore corrupti.
-        Fuga libero distinctio dolores, hic laudantium eaque ipsa tenetur
-        cupiditate fugit dolor reiciendis illum error expedita architecto
-        possimus nostrum eveniet corporis cum rem sapiente, qui totam modi?
-        Quasi magnam debitis reprehenderit! Omnis perferendis dignissimos unde
-        vitae, ab cumque distinctio, veritatis sequi dolores quis explicabo aut
-        facere placeat, veniam fugit quia! Dolorem sequi, laudantium ab
-        aspernatur, recusandae tempora dicta corporis eos excepturi ratione
-        culpa placeat possimus quia nobis laboriosam. Fuga distinctio quaerat
-        facere! Beatae placeat reiciendis tempore nobis vero minima ullam
-        debitis. At saepe beatae reprehenderit consequuntur rem accusantium
-        soluta corrupti nobis? Nisi quis nesciunt quisquam adipisci nihil fugit?
-        Dignissimos consectetur deserunt asperiores neque ad enim cumque,
-        possimus molestiae laborum magni nemo labore reiciendis error facilis
-        aut sunt iste praesentium dolore quia iure, quos soluta facere ut
-        provident. Illum, voluptatem nihil accusantium facilis, maiores animi
-        quaerat vitae pariatur eveniet deserunt cumque quia soluta nostrum ex
-        illo autem id dolor aliquid eligendi officia fugiat aliquam. Facilis
-        numquam dicta beatae iusto voluptates totam dignissimos. Magnam itaque
-        fugit illo rerum corrupti officia exercitationem omnis neque ab debitis,
-        ducimus praesentium. Minima cumque fugiat est corrupti dolore labore
-        pariatur doloremque ea, modi, voluptatum necessitatibus hic esse et ex
-        architecto, molestias ratione laudantium facilis dolor libero sed?
-        Veritatis asperiores voluptatum sapiente necessitatibus, nulla facere
-        nisi. Eaque, officia, vero quos blanditiis saepe, placeat iste nulla
-        porro laudantium deleniti est. Quam enim a dolorum aliquid, perferendis
-        magni recusandae aliquam impedit at minus labore esse, deleniti
-        provident obcaecati exercitationem accusamus eum nihil maiores
-        temporibus facere et ipsum illum quos. Iure ipsum consectetur, rem
-        repellat excepturi saepe, exercitationem dicta ut numquam, amet aperiam
-        corrupti. Praesentium error nemo quis deserunt maiores, odit quod.
-        Repellat pariatur, deserunt itaque laborum ex corporis quasi inventore
-        quam quod est velit nesciunt. Blanditiis, nemo maiores quia eveniet
-        harum ipsam ullam praesentium soluta optio illo numquam cumque enim
-        animi eum officia necessitatibus beatae? Praesentium, aperiam officiis
-        dignissimos, eligendi doloribus expedita doloremque, reprehenderit nobis
-        enim sunt quo voluptatum sint optio illo impedit inventore! Aliquam eius
-        odio similique rerum impedit placeat quod, sed itaque unde perferendis
-        saepe qui quidem commodi fugiat et suscipit ea eum obcaecati. Nemo quo
-        libero vel vero similique, molestiae itaque consequuntur eos corporis
-        nam? Consequatur natus vero numquam aspernatur nam repellat repudiandae
-        consectetur, quas veritatis tenetur. Mollitia sint dolor adipisci
-        quidem. Aliquid iste sapiente, excepturi consequatur nostrum laboriosam
-        atque optio, fugiat consequuntur corrupti voluptate perferendis veniam
-        officia? Ducimus, dicta? Iste voluptas iure in veniam mollitia, ratione
-        hic repellendus suscipit maxime aperiam sapiente error vitae assumenda
-        soluta modi! Cupiditate aliquid numquam, quasi odio rem at aliquam nobis
-        expedita nesciunt saepe ea unde consequatur ipsa ullam facere natus,
-        animi possimus fugit maxime ex dolor aperiam quos tempore! Sed
-        temporibus facilis sapiente consequatur quisquam dolor ipsam animi,
-        assumenda esse!
-      </p>
+    <div id="chat-progress">
+      <div className="chat-top p-5">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex gap-2" id="me">
+            <img src={avatar} alt="" width={36} height={36} id="avatar" />
+            <strong className="pe-2">Me</strong>
+            <span>Nov 10, 2022, 11:12 PM</span>
+          </div>
+          <div id="expand-arrow" onClick={() => setisExpanded(!isExpanded)}>
+            <img
+              src={arrow}
+              alt=""
+              style={{ rotate: isExpanded ? '180deg' : '0deg' }}
+            />
+          </div>
+        </div>
+        <div className="text-center fst-italic text-secondary">
+          You have to started conversation with{' '}
+          <strong>{profile.username}</strong>{' '}
+        </div>
+      </div>
     </div>
   );
 };
