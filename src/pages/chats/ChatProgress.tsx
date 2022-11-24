222f8c9087c4fdf7338974385432808528fba184
rifatptk <rifatptk@gmail.com>
2022-11-24 12:01:15 +0600

    chat qustion system done

--- src/pages/chats/ChatProgress.tsx
+++ src/pages/chats/ChatProgress.tsx
@@ -1,14 +1,22 @@
 import avatar from '../../assets/images/userProfilePage/avatar.png';
 import arrow from '../../assets/images/chatspage/darr.png';
 import { useState } from 'react';
+import Questions from './Questions';
 
 type Props = any;
 
+const questions = [
+  'Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services.',
+  'Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services.',
+  'Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services.',
+  'Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services.',
+  'Leading digital agency with solid design and development expertise. We build readymade websites, mobile applications, and elaborate online business services.',
+];
 const ChatProgress = ({ profile }: Props) => {
   const [isExpanded, setisExpanded] = useState(false);
   return (
-    <div id="chat-progress">
-      <div className="chat-top p-5">
+    <div id="chat-progress" className="p-5">
+      <div className="chat-top">
         <div className="d-flex justify-content-between">
           <div className="d-flex gap-2" id="me">
             <img src={avatar} alt="" width={36} height={36} id="avatar" />
@@ -28,6 +36,9 @@ const ChatProgress = ({ profile }: Props) => {
           <strong>{profile.username}</strong>{' '}
         </div>
       </div>
+
+      {/* questions */}
+      {isExpanded && <Questions questions={questions} />}
     </div>
   );
 };
---
+++ src/pages/chats/Questions.tsx
@@ -0,0 +1,16 @@
+type Props = { questions: string[] };
+
+const Questions = ({ questions }: Props) => {
+  return (
+    <div id="questions" className="p-5">
+      {questions.map((ques, i) => (
+        <div key={i}>
+          <h5>Question 0{i + 1}</h5>
+          <p>{ques}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Questions;
